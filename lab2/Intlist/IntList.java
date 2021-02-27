319709e0534c0a47a6c238c46bbf14cc75d2d609
EsunaFoca <chuqiao.wang.gsm@gmail.com>
2021-02-27 17:08:47 -0500

    fixed null cases for dcatenate()

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if(A == null){
+            A = B;
+        }
         IntList L = A;
         while (L.rest != null) {
             L = L.rest;
