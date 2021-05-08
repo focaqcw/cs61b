9ac56a02be1cf183ef7540fb0145c85d19f89c77
EsunaFoca <chuqiao.wang.gsm@gmail.com>
2021-05-08 18:12:25 -0400

    finished hw2 (roughly)

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,83 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] grid;
+    private int N;
+    private WeightedQuickUnionUF connector;
+
+
+    public Percolation(int N) {    // create N-by-N grid, with all sites initially blocked
+        this.N = N;
+        connector = new WeightedQuickUnionUF(N * N);
+        grid = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = false;
+            }
+        }
+    }
+
+    private int xyTo1D(int r, int c) {
+        return r * N + c;
+    }
+
+    public void open(int row, int col){   // open the site (row, col) if it is not open already
+        grid[row][col] = true;
+
+        if (row != 0){
+            if (grid[row - 1][col]) {
+                connector.union(xyTo1D(row, col), xyTo1D(row - 1, col));
+            }
+        }
+        if (row != N - 1){
+            if (grid[row + 1][col]) {
+                connector.union(xyTo1D(row, col), xyTo1D(row + 1, col));
+            }
+        }
+        if (col != 0 ){
+            if (grid[row][col - 1]) {
+                connector.union(xyTo1D(row, col), xyTo1D(row, col - 1));
+            }
+        }
+        if (col != N - 1){
+            if( grid[row][col + 1]) {
+                connector.union(xyTo1D(row, col), xyTo1D(row, col + 1));
+            }
+        }
+
+    }
+    public boolean isOpen(int row, int col){  // is the site (row, col) open?
+        return grid[row][col];
+    }
+    public boolean isFull(int row, int col){
+        for(int i = 0; i < N; i++){
+            if (connector.find(i) == connector.find(xyTo1D(row,col))
+                & isOpen(row, col)){
+                return true;
+            }
+        }
+        return false;
+
+    }  // is the site (row, col) full?
+    public int numberOfOpenSites(){
+        int cnt = 0;
+        for (int i= 0; i < N*N; i ++){
+            if(grid[i/N][i % N]){
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }           // number of open sites
+    public boolean percolates(){
+        for (int i = 0; i < N; i ++){
+            if (isFull(N-1,i)){
+                return true;
+            }
+        }
+        return false;
+    }              // does the system percolate?
+//        public static void main(String[] args){
+//            return;
+//        }   // use for unit testing (not required)
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,39 @@
 package hw2;
 
 public class PercolationStats {
+    private int[] CntPercList;
+    private int T;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        CntPercList =new int[T];
+        this.T = T;
+        for (int t = 0; t < T; t ++){
+            Percolation SinglePerc = pf.make(N);
+            int[] OpenList = edu.princeton.cs.algs4.StdRandom.permutation(N * N);
+            for(int i = 0; i < N * N; i ++){
+                SinglePerc.open(OpenList[i]/N, OpenList[i]%N);
+                if(SinglePerc.percolates()){
+                    CntPercList[t] = i + 1;
+                    break;
+                }
+            }
+
+        }
+
+
+
+    }  // perform T independent experiments on an N-by-N grid
+    public double mean(){
+        return edu.princeton.cs.algs4.StdStats.mean(CntPercList);
+    }                                           // sample mean of percolation threshold
+    public double stddev(){
+        return edu.princeton.cs.algs4.StdStats.stddev(CntPercList);
+    }                                         // sample standard deviation of percolation threshold
+    public double confidenceLow(){
+        return mean() -1.96 * stddev()/java.lang.Math.sqrt(T);
+    }                                  // low endpoint of 95% confidence interval
+    public double confidenceHigh(){
+        return mean() +1.96 * stddev()/java.lang.Math.sqrt(T);
+    }                                 // high endpoint of 95% confidence interval
 
 }
